fbdb6afe43d43d4d0bdd23cc7293b95957fec915
Kuldeep Dubey <kuldeep.dubey@kelltontech.org>
2022-09-11 10:22:53 +0530

    Login flow all pages added

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -19,7 +19,7 @@ import { LanguageSwitcherAPi } from "../api/account/languageSwitcherAPi";
 
 //import "slick-carousel/slick/slick.css";
 //import "slick-carousel/slick/slick-theme.css";
-//import "../scss/elements/_streaming.scss";
+import "../scss/elements/_streaming.scss";
 
 function MyApp({ Component, pageProps }) {
   const RedirectMaintain = useSelector((s) => s.setting.maintenance);
