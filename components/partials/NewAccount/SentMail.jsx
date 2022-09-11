a05b495761ee007e44e23071e679ab44d177f23a
Kuldeep Dubey <kuldeep.dubey@kelltontech.org>
2022-09-11 09:39:33 +0530

    Login flow all pages added

--- components/partials/NewAccount/SentMail.jsx
+++ components/partials/NewAccount/SentMail.jsx
@@ -8,7 +8,7 @@ export default function ForgetPassword(){
             <div className="back mb32"><IconBack /></div>
             <div className="iconkey mb32"><IconKey /></div>
             <h1 className="title mb8">Check Your Email</h1>
-            <div className="infotext wd328 mb32">We sent a password reset link to aastha.handa@kellton.com</div>
+            <div className="infotext mb32">We sent a password reset link to aastha.handa@kellton.com</div>
             <div className="sent flex space-between">
                 <div className="mb32 already">
                     Don’t receive the email? <Link href="/reset-password"><a>Click to resend</a></Link> 
