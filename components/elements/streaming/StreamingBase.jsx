fd9feff2cdd997bdd3edd726761610f8189eeb77
garima.dadheech <garima.dadheech@kelltontech.com>
2022-09-13 18:39:30 +0530

    added basic functionality to start channel, send and receive msgs

--- components/elements/streaming/StreamingBase.jsx
+++ components/elements/streaming/StreamingBase.jsx
@@ -1,21 +1,74 @@
 import React, {useState, useEffect} from "react";
+import { getToken } from "../../../api/stream/agora";
+import AgoraRTM from "agora-rtm-sdk";
+import { useRouter } from "next/router";
 import Timer from "./Timer";
+import { func } from "prop-types";
+
 
 function StreamingBase() {
   const [open, setOpen] = React.useState(false);
   const [bidAmount, setBidAmount] = React.useState(25);
   const [amountToBid, setAmountToBid] = React.useState(bidAmount+2);
   const [timer, setTimer] = useState("00:00");
+  /*****For notifications *****/
+  const router = useRouter();
+  const hostId = router.query["hostId"];
+  const audienceId = router.query["audienceId"];
+  const [options, setoptions] = useState(null);
+  const userType = hostId ? 'host' : 'audience';
+  const [channel, setChannel] = useState(null);
+  useEffect(() => {
+    if (!options) {
+      setoptions({
+        appID: "eddb3849590747d989047b5b2e213c96",
+        channel: "POKEMON",
+        host: "HOST",
+        audience: "guest" + audienceId,
+      });
+    }
+
+    if (options) {
+      console.log(joinChannel());
+    }
+  }, [options]);
+  const joinChannel = async () => {
+    const client = AgoraRTM.createInstance(options.appID);
+    const token = await getRtmToken(options[userType]);
+    await client.login({ uid: options[userType], token });
+    const channel = await client.createChannel(options.channel);
+    console.log(channel);
+    await channel.join();
+    setChannel(channel);
+    channel.on('Member Joined', function(memberId){
+        console.log(memberId+"JOined")
+    });
+    return channel;
+  };
+  const getRtmToken = async (uuid) => {
+    const url = `/stream/getStreamToken?token=RTM&uid=${uuid}`;
+    const response = await getToken(url);
+    return response.rtmToken;
+  };
+  /*****End notifications *****/
   const handleMuteButton = () => {
   };
   const handleCustomBid = () => {
     setOpen(true);
   };
-
-  const handleConfirmBid = () => {
-    setBidAmount(amountToBid);
-    setAmountToBid(amountToBid+2);
+  const handleConfirmBid = async () => {
     setOpen(false);
+    let message = String(amountToBid)
+    await channel.sendMessage({text: message, type: "text"}).then(()=>{
+        console.log("message", message)
+    })
+
+    channel.on('ChannelMessage', function(message, memberId){
+        console.log("hehehehheh")
+        console.log("message:" + JSON.stringify(message) + "received from memeber:" + memberId)
+        setBidAmount(amountToBid);
+        setAmountToBid(amountToBid+2);
+    })
   }
   useEffect(() => {
     let minutes = 0;
