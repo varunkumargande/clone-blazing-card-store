0138555787c20e2c2e1470c5473615e2c71d16f4
whyyouman <jayant1998.sha@gmail.com>
2022-09-14 13:46:53 +0530

    feat: add card and list card feature

--- api/auth/login.js
+++ api/auth/login.js
@@ -8,29 +8,6 @@ import { modalSuccess, modalWarning } from "../intercept";
 import { addItem } from '../../store/cart/action'
 
 export async function UserLogin(email, password, loginType, Router, setLoginError, dispatch, setMail, setPassword, setLoadImg) {
-
-    // return fetch(apiUrl+'customer/login', {
-    //     method: 'POST',
-    //     body: JSON.stringify({
-    //         'emailId':email, 'password': password,"type":loginType
-    //     })
-    // })
-    //     .then((response) => {
-    //             if (response.status === 1) {
-    //                 sessionStorage.setItem("spurtToken", response.data.token);
-    //                 // sessionStorage.setItem("spurtUser",JSON.stringify(response.data.user));
-    //                 getProfileApi( )
-    //                 modalSuccess('success',response.message)
-    //                 Router.push('/');
-    //                 cartListApi(dispatch)
-    //             } else {
-    //                 setLoginError(response.message)
-    //                 modalWarning('error',response.message)
-    //                 setMail("")
-    //                 setPassword("")
-    //             }
-    //     })
-
     const data = JSON.stringify({
         emailId: email,
         password: password,
@@ -38,9 +15,7 @@ export async function UserLogin(email, password, loginType, Router, setLoginErro
     })
     const result = await APIServices.create('customer/login', data)
     if (result && result.data && result.data.status === 1) {
-
         sessionStorage.setItem("spurtToken", result.data.data.token);
-        // sessionStorage.setItem("spurtUser",JSON.stringify(response.data.user));
         getProfileApi()
         modalSuccess('success', result.data.message)
         Router.push('/');
@@ -52,5 +27,4 @@ export async function UserLogin(email, password, loginType, Router, setLoginErro
         setPassword("")
     }
     setLoadImg(false)
-
 };
