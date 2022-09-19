3b210cb190e8cf0a82ab46f27e97d52d98923efc
rohit gautam <rohit.gautam@kelltontech.com>
2022-09-19 18:08:27 +0530

    Issue Fixes and code optimization

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/partials/LandingPage/Header";
 import MobileHeader from "../components/partials/LandingPage/MobileHeader";
 import Category from "../components/partials/LandingPage/Category";
@@ -10,46 +10,60 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { categoryApi } from "../api/category/category";
 import Electronic from "../components/partials/LandingPage/Electronic";
+import { element } from "prop-types";
 
+export default function landingpage() {
+  const [windowWidth, setWindowWidth] = useState(0);
+  let resizeWindow = () => {
+    setWindowWidth(window.innerWidth);
+  };
+  const categories = useSelector(
+    (state) => state?.stream?.streamdetails?.category
+  );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    resizeWindow();
+    window.addEventListener("resize", resizeWindow);
+    return () => window.removeEventListener("resize", resizeWindow);
+  }, []);
 
-export default function landingpage(){
-    const [windowWidth, setWindowWidth] = useState(0);
-    let resizeWindow = () => {
-      setWindowWidth(window.innerWidth);
-    };
-    const categories = useSelector((state)=>state?.category?.categories)
-    const dispatch = useDispatch();
-    console.log("landing page", categories)
-    useEffect(() => {
-      resizeWindow();
-      window.addEventListener("resize", resizeWindow);
-      return () => window.removeEventListener("resize", resizeWindow);
-    }, []);
+  useEffect(() => {
+    categoryApi(dispatch);
+  }, []);
 
-    useEffect(()=>{
-      console.log(categories,"landingpage")
-      categoryApi(dispatch);
-    },[])
+  // const getCatStream=()=>{
+  //   return categories?.map((cat)=>{
+  //     return <div className="card-wrapper">
+  //     <LiveShow name={cat?.name} catId={cat?.categoryId}/>
+  // </div>
+  //   })
+  // }
+  if (categories) {
+    console.log("landing page categories", categories);
+  }
 
-    // const getCatStream=()=>{
-    //   return categories?.map((cat)=>{
-    //     return <div className="card-wrapper">
-    //     <LiveShow name={cat?.name} catId={cat?.categoryId}/>
-    // </div>
-    //   })
-    // }
-    return(
-        <div className="home-container">
-            {windowWidth <= 1024 ? <MobileHeader/> : <Header />}
-            <Category />
-            <div className="card-wrapper">
-                <LiveShow />
-                <ScheduledShow />
-                <Electronic />
-                <Jewellery />
-            </div>
-            {/* {getCatStream()} */}
-            <Footer />
-        </div>
-    );
+  const getAllCategoriesCard = () => {
+    if (categories) {
+      const categoriesData = Object.entries(categories);
+      return categoriesData.map((element) => {
+        return <Electronic categoryData={element} />;
+      });
+    }
+  };
+
+  return (
+    <div className="home-container">
+      {windowWidth <= 1024 ? <MobileHeader /> : <Header />}
+      <Category />
+      <div className="card-wrapper">
+        <LiveShow />
+        <ScheduledShow />
+        {getAllCategoriesCard()}
+        {/* <Electronic /> */}
+        {/* <Jewellery /> */}
+      </div>
+      {/* {getCatStream()} */}
+      <Footer />
+    </div>
+  );
 }
