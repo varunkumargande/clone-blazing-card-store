2b4c1bed2440ac9c689bcfad166b79aab44de293
Shrey Shah <shrey.shah@kellton.com>
2022-10-04 15:06:19 +0530

    Added dynamic nature in Profle/index.jsx file

--- api/profile/ProfileMethods.js
+++ api/profile/ProfileMethods.js
@@ -1,74 +1,72 @@
 import http from '../intercept';
 
-let GetPublicProfile = async (userId) => {
-    if(userId) {
-        const response = await http.get(`customer/get-public-profile/${userId}`);
-        if(response && response.data) {
-            return response;
-        } 
-        return false;
-    } else {
-        return false;
-    }
+let GetPublicProfile = async (userId, callback) => {
+    http.get(`customer/get-public-profile/${userId}`)
+    .then((res) => {
+        functionCallbackSetter(res, callback);
+    })
+    .catch((e) => {
+        console.log(e.response);
+    })
 }
 
-let GetScheduledStreams = async (userId) => {
-    if(userId) {
-        const response = await http.get(`/stream/vendorProfile-scheduled-stream?vendorId=${userId}`);
-        if(response && response.data) {
-            return response;
-        } 
-        return false;
-    } else {
-        return false;
-    }
+let GetScheduledStreams = async (userId, callback) => {
+    http.get(`/stream/vendorProfile-scheduled-stream?vendorId=${userId}`)
+    .then((res) => {
+        functionCallbackSetter(res, callback);
+    })
+    .catch((e) => {
+        console.log(e.response);
+    })
 }
 
-let GetPreviousStreams = async (userId) => {
-    if(userId) {
-        const response = await http.get(`/stream/vendorProfile-previous-stream?vendorId=${userId}`);
-        if(response && response.data) {
-            return response;
-        } 
-        return false;
-    } else {
-        return false;
-    }
+let GetPreviousStreams = async (userId, callback) => {
+    http.get(`/stream/vendorProfile-previous-stream?vendorId=${userId}`)
+    .then((res) => {
+        functionCallbackSetter(res, callback);
+    })
+    .catch((e) => {
+        console.log(e.response);
+    })
 }
 
-let GetLikedStreams = async (userId) => {
-    if(userId) {
-        const response = await http.get(`/stream/liked-streams?user_id=${userId}`);
-        if(response && response.data) {
-            return response;
-        } 
-        return false;
-    } else {
-        return false;
-    }
+let GetLikedStreams = (userId, callback) => {
+    http.get(`/stream/liked-streams?user_id=${userId}`)
+    .then((res) => {
+        functionCallbackSetter(res, callback);
+    })
+    .catch((e) => {
+        console.log(e.response);
+    })
 }
 
-let GetUserFollowers = async (userId) => {
-    if(userId) {
-        const response = await http.get(`/follow/get_follower?user_id=${userId}`);
-        if(response && response.data && response.status === 200) {
-            return response;
-        } 
-        return false;
-    } else {
-        return false;
-    }
+let GetUserFollowers = async (userId, callback) => {
+    http.get(`/follow/get_follower?user_id=${userId}`)
+    .then((res) => {
+        functionCallbackSetter(res, callback);
+    })
+    .catch((e) => {
+        console.log(e.response);
+    })
 }
 
-let GetUserFollowings = async (userId) => {
-    if(userId) {
-        const response = await http.get(`/follow/get_following?user_id=${userId}`);
-        if(response && response.data) {
-            return response.data;
-        } 
-        return false;
-    } else {
-        return false;
+let GetUserFollowings = async (userId, callback) => {
+    http.get(`/follow/get_following?user_id=${userId}`)
+    .then((res) => {
+        functionCallbackSetter(res, callback);
+    })
+    .catch((e) => {
+        console.log(e.response);
+    })
+}
+
+let functionCallbackSetter = (response, callback) => {
+    if (response && response.status == 200) {
+        if (response.data.data) {
+            callback(response.data.data);
+        } else {
+            callback([]);
+        }
     }
 }
 
