7115019cb7feee24eae708c6e871c6052a3db6b7
rohit gautam <rohit.gautam@kelltontech.com>
2022-10-04 16:31:56 +0530

    UI fixes - color pallete removed _ Image issue fix _ Bread crum

--- components/shared/headers/HeaderDefault.jsx
+++ components/shared/headers/HeaderDefault.jsx
@@ -35,6 +35,7 @@ function HeaderDefault({ auth }, props) {
   const [aimg, setAimg] = useState("");
   const [email, setEmail] = useState("");
   let { pageName } = router.query;
+  let userData = JSON.parse(sessionStorage.getItem("spurtUser"));
 
   const authFunc = () => {
     if (sessionStorage.getItem("spurtToken") !== null) {
@@ -141,7 +142,32 @@ function HeaderDefault({ auth }, props) {
                 <button className="profile">
                   <span onClick={handleOnClick}>
 
-                    <img src={aimg} alt="Profile" />
+                    {/* <img src={aimg} alt="Profile" /> */}
+                    {userData != null ? (
+                      <>
+                        {userData.avatar != null && userData.avatarPath != null ? (
+                          <>
+                            <img
+                              src={
+                                imageUrl +
+                                "?path=" +
+                                userData.avatarPath +
+                                "&name=" +
+                                userData.avatar +
+                                "&width=500&height=500"
+                              }
+                              alt="Profile"
+                            />
+                          </>
+                        ) : (
+                          <>
+                            <img src={"/static/img/no-image.png"} />
+                          </>
+                        )}
+                      </>
+                    ) : (
+                      ""
+                    )}
 
                     <IconDropdown />
                   </span>
