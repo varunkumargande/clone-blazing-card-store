54146abccb04f8226587bc59c9cfd7cd33a9beba
garima.dadheech <garima.dadheech@kelltontech.com>
2022-10-06 14:07:23 +0530

    left div only be visible on clicking shops icon on mobile screen and closes when buyer clicks somewhere else

--- components/elements/streaming/index.jsx
+++ components/elements/streaming/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import LeftDiv from "./LeftDiv";
 import RightDiv from "./RightDiv";
 import CenterDiv from "./CenterDiv";
@@ -7,7 +7,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { streamData } from "../../../store/stream/action";
 import { io } from "socket.io-client";
 import { socketIO } from "../../../api/url";
-
 
 function Index() {
   const [open, setOpen] = useState(false);
@@ -20,9 +19,7 @@ function Index() {
   const [openPayment, setOpenPayment] = useState(false);
   const [productDetail, setProductDetail] = useState([]);
   const dispatch = useDispatch();
-  const [socketObject, setSocketObject] = useState(
-    io(socketIO)
-  );
+  const [socketObject, setSocketObject] = useState(io(socketIO));
   const [auctionNotification, setAuctionNotification] = useState(null);
   const [bidNotification, setBidNotification] = useState(null);
   const stream = useSelector((state) => {
@@ -31,7 +28,8 @@ function Index() {
   const [currentAuction, setCurrentAuction] = useState(null);
   const streamingDetails = stream?.streamData;
   const streamPageData = stream?.streamPageData;
-
+  const [isLeftDivOpen, setLeftDivOpen] = useState();
+  
   useEffect(() => {
     socketObject.on(`${uuid}-bid`, (bid) => {
       setBidNotification(bid);
@@ -42,22 +40,29 @@ function Index() {
     dispatch(streamData(uuid));
   }, []);
 
+  //Method to show and hide left div
+  const handleLeftDiv = (toggle) => {
+    setLeftDivOpen(toggle);
+  };
+
   return (
     <>
       {streamingDetails?.uuid ? (
         <>
           <div className="streaming-page flex space-between">
-            <LeftDiv
-              auctionNotification={auctionNotification}
-              open={open}
-              productDetail={setProductDetail}
-              openPayment={setOpenPayment}
-              setOpen={setOpen}
-              addShippInfo={addShippInfo}
-              addPayInfo={addPayInfo}
-              setCustomerId={setCustomerId}
-              streamingDetails={streamingDetails}
-            />
+              <LeftDiv
+                auctionNotification={auctionNotification}
+                open={open}
+                productDetail={setProductDetail}
+                openPayment={setOpenPayment}
+                setOpen={setOpen}
+                addShippInfo={addShippInfo}
+                addPayInfo={addPayInfo}
+                setCustomerId={setCustomerId}
+                streamingDetails={streamingDetails}
+                handleLeftDiv={handleLeftDiv}
+                isLeftDivOpen={isLeftDivOpen}
+              />
             <CenterDiv
               open={open}
               productDetail={productDetail}
@@ -70,6 +75,7 @@ function Index() {
               streamDetails={selectedStream}
               currentAuction={currentAuction}
               streamingDetails={streamingDetails}
+              handleLeftDiv={handleLeftDiv}
             />
             <RightDiv
               streamingDetails={streamingDetails}
