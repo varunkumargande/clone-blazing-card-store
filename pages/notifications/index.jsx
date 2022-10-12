d763efc6978000c4ff3e0e041755d2970b00437d
Kuldeep Dubey <kuldeep.dubey@kelltontech.org>
2022-10-12 19:06:11 +0530

    Issues fixes

--- pages/notifications/index.jsx
+++ pages/notifications/index.jsx
@@ -4,10 +4,7 @@ import Notifications from '../../components/partials/Notifications/Notifications
 export default function notifications(){
     return(
         <div className="notification-wrapper">
-            <div className="head-title flex space-between flex-center">
-                <h1>Notification</h1>
-                {/* <div className="unread">Show All Unread</div> */}
-            </div>
+            <h1>Notification</h1>
             <Notifications />
          </div>
     );
