dd96dbf69ed0441d430f5b83fe2ef77948716b8f
lokesh sirari <lokesh.sirari@Kellggncpu0423.local>
2022-10-13 12:47:47 +0530

    removed console.log

--- api/common/common.js
+++ api/common/common.js
@@ -2,7 +2,6 @@ import APIServices from '../../services'
 
 export const getImageSignedUrl = async (payLoad) => {
   const result = await APIServices.put(`media/get-presigned-url-s3?path=${payLoad.path}&key=${payLoad.key}`)
-  console.log(result)
   if (result && result.data && result.data) {
     return result.data;
   }
