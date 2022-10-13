6976fda11addd6e62ed3e72cec2c52b1b183cde6
Devendra Singh <devendra.singh@kelltontech.com>
2022-10-13 14:48:05 +0000

    Update url.js

--- api/url.js
+++ api/url.js
@@ -1,8 +1,8 @@
-export const apiUrl='https://blazing-card-backend-dev.kellton.net/api' //http://localhost:8000/api/'
-export const imageUrl='https://blazing-card-backend-dev.kellton.net/api/media/image-resize'
+export const apiUrl='https://blazing-card-backend-qa.kellton.net/api' //http://localhost:8000/api/'
+export const imageUrl='https://blazing-card-backend-qa.kellton.net/api/media/image-resize'
 export const env='development'
 export const port = "3000"
-export const videoUrl='https://blazing-card-backend-dev.kellton.net/api/media/video-preview-s3/' //http://localhost:9000
+export const videoUrl='https://blazing-card-backend-qa.kellton.net/api/media/video-preview-s3/' //http://localhost:9000
 export const agora="http://localhost:8080";
-export const socketIO ="https://blazing-card-notification-dev.kellton.net/"
-export const apiBaseUrl='https://blazing-card-backend-dev.kellton.net'
+export const socketIO ="https://blazing-card-notification-qa.kellton.net/"
+export const apiBaseUrl='https://blazing-card-backend-qa.kellton.net'
