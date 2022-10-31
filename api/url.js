b0a64a06bc47027cc2ba5dd04fcf7fb06fc86df0
Gande Varun Kumar <gande.kumar@kelltontech.com>
2022-10-31 15:09:34 +0000

    Update api/url.js

--- api/url.js
+++ api/url.js
@@ -1,11 +1,11 @@
-export const apiUrl='https://blazing-card-backend-dev.kellton.net/api' //http://localhost:8000/api/'
+export const apiUrl='https://blazing-card-backend-qa.kellton.net/api' //http://localhost:8000/api/'
 // export const apiUrl='https://ead8-110-235-216-15.in.ngrok.io/api' //http://localhost:8000/api/'
-export const imageUrl='https://blazing-card-backend-dev.kellton.net/api/media/image-resize'
+export const imageUrl='https://blazing-card-backend-qa.kellton.net/api/media/image-resize'
 export const env='development'
 export const port = "3000"
-export const videoUrl='https://blazing-card-backend-dev.kellton.net/api/media/video-preview-s3/' //http://localhost:9000
+export const videoUrl='https://blazing-card-backend-qa.kellton.net/api/media/video-preview-s3/' //http://localhost:9000
 export const agora="http://localhost:8080";
-export const notificationBaseUrl ="https://blazing-card-backend-dev.kellton.net/events/start?channel="
-export const apiBaseUrl='https://blazing-card-backend-dev.kellton.net'
+export const notificationBaseUrl ="https://blazing-card-backend-qa.kellton.net/events/start?channel="
+export const apiBaseUrl='https://blazing-card-backend-qa.kellton.net'
 export const cloudImageUrl = 'https://res.cloudinary.com/dwwbiyabf/image/upload/';
 export const cloudFolder = 'blazing';
