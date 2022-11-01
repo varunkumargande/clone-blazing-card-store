b5b5a8119b554a9654ce872b4edf37b4c00d44e4
Varun Kumar Gande <babluvarun@gmail.com>
2022-11-01 14:10:13 +0530

    env files test 1

--- api/url.js
+++ api/url.js
@@ -1,16 +1,3 @@
-// export const apiUrl = process.env.NEXT_PUBLIC_API_URL; //http://localhost:8000/api/'
-// // export const apiUrl='https://ead8-110-235-216-15.in.ngrok.io/api' //http://localhost:8000/api/'
-// export const imageUrl = process.env.NEXT_PUBLIC_IMAGE_URL;
-// export const env = process.env.NEXT_PUBLIC_ENV;
-// export const port = process.env.NEXT_PUBLIC_PORT;
-// export const videoUrl = process.env.NEXT_PUBLIC_VIDEO_URL; //http://localhost:9000
-// export const agora = process.env.NEXT_PUBLIC_AGORA;
-// export const notificationBaseUrl =
-//   process.env.NEXT_PUBLIC_NOTIFICATION_BASE_URL;
-// export const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-// export const cloudImageUrl = process.env.NEXT_PUBLIC_CLOUD_IMAGE_URL;
-// export const cloudFolder = process.env.NEXT_PUBLIC_CLOUD_FOLDER;
-
 export const apiUrl='https://blazing-card-backend-dev.kellton.net/api' //http://localhost:8000/api/'
 // export const apiUrl='https://ead8-110-235-216-15.in.ngrok.io/api' //http://localhost:8000/api/'
 export const imageUrl='https://blazing-card-backend-dev.kellton.net/api/media/image-resize'
@@ -23,4 +10,4 @@ export const apiBaseUrl='https://blazing-card-backend-dev.kellton.net'
 export const cloudImageUrl = 'https://res.cloudinary.com/dwwbiyabf/image/upload/';
 export const cloudFolder = 'blazing';
 
-console.log("varun urls", process.env.NEXT_PUBLIC_ENV)
+console.log("varun", process.env.NEXT_PUBLIC_TEST)
