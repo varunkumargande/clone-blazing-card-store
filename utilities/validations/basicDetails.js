20157d51e34c02a088fe3c9ad2af765291cad674
Garima Dadheech <garima.dadheech@kelltontech.com>
2022-11-01 13:05:54 +0000

    added validation on basic details and shipping details become a seller

--- utilities/validations/basicDetails.js
+++ utilities/validations/basicDetails.js
@@ -1,8 +1,9 @@
 import * as yup from "yup";
+import { regex } from "../../components/Constants/regex";
 
 export const basicDetailvalidation = yup.object().shape({
-  fullName: yup.string().required('Please Enter Your Full name'),
-  uniqueId: yup.number().typeError("Invalid Details Entered").required("Please Enter your UniqueId"),
+  fullName: yup.string().matches(regex.onlyAlphabetsBothCases, 'Please enter letters only').required('Please Enter Your Full name'),
+  uniqueId: yup.string().matches(regex.onlyNumbers, 'Please enter digits only').min(9, "Digits should be equal to 9").required("Please Enter your UniqueId"),
   upload: yup
     .mixed()
     .required("Please upload identification document")
