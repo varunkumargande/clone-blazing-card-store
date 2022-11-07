3aebb0f8759a498d7d3a75957b70b0fc5538335b
Gande Varun Kumar <gande.kumar@kelltontech.com>
2022-11-07 15:43:05 +0000

    Streaming Skelton enabled

--- api/stream/getToken.js
+++ api/stream/getToken.js
@@ -1,14 +1,14 @@
 import { modalSuccess, modalWarning } from "../intercept";
-import APIServices from '../../services';
+import APIServices from "../../services";
 
 export async function getToken(token, channel, uid, tokentype, role) {
-    const result =await APIServices.get(`stream/getStreamToken?token=${token}&channel=${channel}&uid=${uid}&tokentype=${tokentype}&role=${role}`)
-    if(result&&result.data&&result.data.status===1){
-        modalSuccess('success',result.data.message)
-        return result.data.rtmToken
-    }
-    else{
-        modalWarning('error',result.data.message);
-    }
+  const result = await APIServices.get(
+    `stream/getStreamToken?token=${token}&channel=${channel}&uid=${uid}&tokentype=${tokentype}&role=${role}`
+  );
+  if (result?.data?.status === 1) {
+    modalSuccess("success", result?.data?.message);
+    return result?.data?.rtmToken;
+  } else {
+    modalWarning("error", result?.data?.message);
+  }
 }
-
