785a2f3d6ba9ac66a56f4cefb675361e697841cd
Varun Kumar Gande <babluvarun@gmail.com>
2022-11-08 18:07:29 +0530

    ShippingInformation fixes

--- components/partials/EditProfile/ShippingInformation.jsx
+++ components/partials/EditProfile/ShippingInformation.jsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from "react";
-import IconDelete from "../../Icons/IconDelete";
-import Link from "next/link";
+import React, { memo, useEffect, useState } from "react";
 import { addressListApi } from "../../../api";
 import { useDispatch, useSelector } from "react-redux";
 import { Formik, setNestedObjectValues } from "formik";
@@ -12,7 +10,7 @@ import ErrorMessage from "../../CommonComponents/ErrorMessage";
 import { getStateList } from "../../../api/common/common";
 import { US_CODE } from "../../Constants";
 
-export default function ShippingDetails() {
+function ShippingDetails() {
   const dispatch = useDispatch();
   const [addressData, setAddressData] = useState([]);
   const [addressLoader, setAddressLoader] = useState(false);
@@ -31,7 +29,7 @@ export default function ShippingDetails() {
   };
   const apiCallFunc = () => {
     countryListApi(setCountryData);
-    getStateList(setStateList)
+    getStateList(setStateList);
   };
 
   const initialShippingValues = {
@@ -50,7 +48,9 @@ export default function ShippingDetails() {
     // address2: Yup.string().required("Required"),
     city: Yup.string().required("Required"),
     state: Yup.string().required("Required"),
-    postcode: Yup.number().required("Required").typeError('Please enter digits only'),
+    postcode: Yup.number()
+      .required("Required")
+      .typeError("Please enter digits only"),
     countryId: Yup.string().required("Required"),
     company: Yup.string().required("Required"),
   });
@@ -60,153 +60,145 @@ export default function ShippingDetails() {
       <h3>Shipping Information</h3>
 
       {addressLoader ? (
-        <>
-          <Loader />
-        </>
+        <Loader />
       ) : (
-        <>
-          <Formik
-            initialValues={initialShippingValues}
-            validationSchema={shippingSchema}
-            onSubmit={(values) => {
-              setAddressLoader(true);
-              if (addressData.length != 0) {
-                editAddressApi(
-                  values,
-                  addressData[0].addressId,
-                  setAddressLoader,
-                  addressList,
-                  dispatch
-                );
-              } else {
-                UserAddAddress(
-                  values,
-                  dispatch,
-                  setAddressLoader,
-                  addressList,
-                );
-              }
-            }}
-          >
-            {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting,
-              setValues,
-            }) => (
-                <form onSubmit={handleSubmit}>
-                  <div className="box">
-                    <div className="inner-box">
-                      <div className="discriptionlg">
-                        A return address must be added before going live on
-                        Blazing Cards. This will be used on your shipment
-                        labels.
-                      </div>
-                      <div className="flex space-between">
-                        <div className="input-control">
-                          <label>Full Name *</label>
-                          <input
-                            name="company"
-                            placeholder={"Enter here"}
-                            className="grey-bg"
-                            onChange={handleChange}
-                            value={values.company}
-                            maxLength={50}
-                          />
-                          <ErrorMessage
-                            errors={errors.company}
-                            touched={touched.company}
-                          />
-                        </div>
-                        <div className="input-control wd50">
-                          <label>Address Line 1 *</label>
-                          <input
-                            name="address1"
-                            placeholder={"Enter here"}
-                            className="grey-bg"
-                            onChange={handleChange}
-                            value={values.address1}
-                          />
-                          <ErrorMessage
-                            errors={errors.address1}
-                            touched={touched.address1}
-                          />
-                        </div>
-                        <div className="input-control wd50">
-                          <label htmlFor="usr">Address Line 2</label>
-                          <input
-                            name="address2"
-                            placeholder={"Enter here"}
-                            id="usr"
-                            className="grey-bg"
-                            onChange={handleChange}
-                            value={values.address2}
-                          />
-                          {/* <span className="errorMessage">{errors.address2}</span> */}
-                        </div>
-                        <div className="input-control wd50">
-                          <label htmlFor="usr">Country *</label>
-                          <select
-                            className="grey-bg"
-                            name="countryId"
-                            onChange={handleChange}
-                            value={values.countryId}
-                            disabled={true}
-                          >
-                            {/* <option>Select</option> */}
-                            <option>United States</option>
-                            {countryData.map((item, index) => {
-                              return (
-                                <>
-                                  <option value={item.countryId}>
-                                    {item.name}
-                                  </option>
-                                </>
-                              );
-                            })}
-                          </select>
-                          <ErrorMessage
-                            errors={errors.countryId}
-                            touched={touched.countryId}
-                          />
-                        </div>
-                        <div className="input-control wd50">
-                          <label htmlFor="usr">Postal Code *</label>
-                          <input
-                            name="postcode"
-                            placeholder={"Enter here"}
-                            id="usr"
-                            className="grey-bg"
-                            onChange={handleChange}
-                            value={values.postcode}
-                          />
-                          <ErrorMessage
-                            errors={errors.postcode}
-                            touched={touched.postcode}
-                          />
-                        </div>
-                        <div className="input-control wd50">
-                          <label htmlFor="usr">City *</label>
-                          <input
-                            name="city"
-                            placeholder={"Enter here"}
-                            id="usr"
-                            className="grey-bg"
-                            onChange={handleChange}
-                            value={values.city}
-                          />
-                          <ErrorMessage
-                            errors={errors.city}
-                            touched={touched.city}
-                          />
-                        </div>
-                        <div className="input-control wd50">
-                          <label htmlFor="usr">State *</label>
-                          {/* <input
+        <Formik
+          initialValues={initialShippingValues}
+          enableReinitialize
+          validationSchema={shippingSchema}
+          onSubmit={(values) => {
+            setAddressLoader(true);
+            if (addressData.length != 0) {
+              editAddressApi(
+                values,
+                addressData[0].addressId,
+                setAddressLoader,
+                addressList,
+                dispatch
+              );
+            } else {
+              UserAddAddress(values, dispatch, setAddressLoader, addressList);
+            }
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            setValues,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <div className="box">
+                <div className="inner-box">
+                  <div className="discriptionlg">
+                    A return address must be added before going live on Blazing
+                    Cards. This will be used on your shipment labels.
+                  </div>
+                  <div className="flex space-between">
+                    <div className="input-control">
+                      <label>Full Name *</label>
+                      <input
+                        name="company"
+                        placeholder={"Enter here"}
+                        className="grey-bg"
+                        onChange={handleChange}
+                        value={values.company}
+                        maxLength={50}
+                      />
+                      <ErrorMessage
+                        errors={errors.company}
+                        touched={touched.company}
+                      />
+                    </div>
+                    <div className="input-control wd50">
+                      <label>Address Line 1 *</label>
+                      <input
+                        name="address1"
+                        placeholder={"Enter here"}
+                        className="grey-bg"
+                        onChange={handleChange}
+                        value={values.address1}
+                      />
+                      <ErrorMessage
+                        errors={errors.address1}
+                        touched={touched.address1}
+                      />
+                    </div>
+                    <div className="input-control wd50">
+                      <label htmlFor="usr">Address Line 2</label>
+                      <input
+                        name="address2"
+                        placeholder={"Enter here"}
+                        id="usr"
+                        className="grey-bg"
+                        onChange={handleChange}
+                        value={values.address2}
+                      />
+                      {/* <span className="errorMessage">{errors.address2}</span> */}
+                    </div>
+                    <div className="input-control wd50">
+                      <label htmlFor="usr">Country *</label>
+                      <select
+                        className="grey-bg"
+                        name="countryId"
+                        onChange={handleChange}
+                        value={values.countryId}
+                        disabled={true}
+                      >
+                        {/* <option>Select</option> */}
+                        <option>United States</option>
+                        {countryData.map((item, index) => {
+                          return (
+                            <>
+                              <option value={item.countryId}>
+                                {item.name}
+                              </option>
+                            </>
+                          );
+                        })}
+                      </select>
+                      <ErrorMessage
+                        errors={errors.countryId}
+                        touched={touched.countryId}
+                      />
+                    </div>
+                    <div className="input-control wd50">
+                      <label htmlFor="usr">Postal Code *</label>
+                      <input
+                        name="postcode"
+                        placeholder={"Enter here"}
+                        id="usr"
+                        className="grey-bg"
+                        onChange={handleChange}
+                        value={values.postcode}
+                      />
+                      <ErrorMessage
+                        errors={errors.postcode}
+                        touched={touched.postcode}
+                      />
+                    </div>
+                    <div className="input-control wd50">
+                      <label htmlFor="usr">City *</label>
+                      <input
+                        name="city"
+                        placeholder={"Enter here"}
+                        id="usr"
+                        className="grey-bg"
+                        onChange={handleChange}
+                        value={values.city}
+                      />
+                      <ErrorMessage
+                        errors={errors.city}
+                        touched={touched.city}
+                      />
+                    </div>
+                    <div className="input-control wd50">
+                      <label htmlFor="usr">State *</label>
+                      {/* <input
                             name="state"
                             placeholder={"Enter here"}
                             id="usr"
@@ -214,50 +206,49 @@ export default function ShippingDetails() {
                             onChange={handleChange}
                             value={values.state}
                           /> */}
-                          <select
-                            className="grey-bg"
-                            name="state"
-                            onChange={handleChange}
-                            value={values.state}
-                          >
-                            <option>Select</option>
-                            {stateList.map((item, index) => {
-                              return (
-                                <>
-                                  <option value={item.name}>
-                                    {item.name}
-                                  </option>
-                                </>
-                              );
-                            })}
-                          </select>
-                          <ErrorMessage
-                            errors={errors.state}
-                            touched={touched.state}
-                          />
-                        </div>
-                      </div>
+                      <select
+                        className="grey-bg"
+                        name="state"
+                        onChange={handleChange}
+                        value={values.state}
+                      >
+                        <option>Select</option>
+                        {stateList.map((item, index) => {
+                          return (
+                            <>
+                              <option value={item.name}>{item.name}</option>
+                            </>
+                          );
+                        })}
+                      </select>
+                      <ErrorMessage
+                        errors={errors.state}
+                        touched={touched.state}
+                      />
                     </div>
                   </div>
-                  <div className="button-wrapper flex mb40">
-                    <button
-                      className="border-btn mr16"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        setValues(initialShippingValues, true);
-                      }}
-                    >
-                      Cancel
-                    </button>
-                    <button className="primary-btn" onSubmit={handleSubmit}>
-                      Save
-                    </button>
-                  </div>
-                </form>
-              )}
-          </Formik>
-        </>
+                </div>
+              </div>
+              <div className="button-wrapper flex mb40">
+                <button
+                  className="border-btn mr16"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setValues(initialShippingValues, true);
+                  }}
+                >
+                  Cancel
+                </button>
+                <button className="primary-btn" onSubmit={handleSubmit}>
+                  Save
+                </button>
+              </div>
+            </form>
+          )}
+        </Formik>
       )}
     </div>
   );
 }
+
+export default memo(ShippingDetails);
