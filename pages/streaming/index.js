6ca7f5e8717f2fe52db1a7a0aaa130b9626a4b6e
Varun Kumar Gande <babluvarun@gmail.com>
2022-11-22 15:31:09 +0530

    bug commented

--- pages/streaming/index.js
+++ pages/streaming/index.js
@@ -10,7 +10,8 @@ import IconBack from "../../components/Icons/IconBack";
 import { useIsMobile } from "../../contexts/Devices/CurrentDevices";
 import Head from "next/head";
 
-const StreamingPage = (data) => {
+const StreamingPage = ({ data }) => {
+  // console.log("data", data);
   const SeoData = data?.data?.data;
   const network = useNetwork();
   const Streaming = dynamic(
@@ -33,23 +34,28 @@ const StreamingPage = (data) => {
 
   return (
     <>
-      <Head>
-        <title>{SeoData?.title}</title>
-        <meta name="description" content={SeoData?.description} />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta property="og:title" content={SeoData?.title} />
-        <meta property="og:description" content={SeoData?.description} />
-        <meta
-          property="og:image"
-          content={`${process.env.NEXT_PUBLIC_CLOUD_IMAGE_URL}w_280,h_398,c_fill,q_auto,fl_any_format,dpr_auto/blazing/${SeoData?.image_path}/${SeoData?.preview_image}`}
-        />
-        <meta property="og:image:width" content="560" />
-        <meta property="og:image:height" content="796" />
-        <link
-          rel="shortcut icon"
-          href={`${process.env.NEXT_PUBLIC_CLOUD_IMAGE_URL}w_280,h_398,c_fill,q_auto,fl_any_format,dpr_auto/blazing/${SeoData?.image_path}/${SeoData?.preview_image}`}
-        />
-      </Head>
+      {SeoData && (
+        <Head>
+          <title>{SeoData?.title}</title>
+          <meta name="description" content={SeoData?.description} />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta property="og:title" content={SeoData?.title} />
+          <meta property="og:description" content={SeoData?.description} />
+          <meta
+            property="og:image"
+            content={`${process.env.NEXT_PUBLIC_CLOUD_IMAGE_URL}w_280,h_398,c_fill,q_auto,fl_any_format,dpr_auto/blazing/${SeoData?.image_path}/${SeoData?.preview_image}`}
+          />
+          <meta property="og:image:width" content="560" />
+          <meta property="og:image:height" content="796" />
+          <link
+            rel="shortcut icon"
+            href={`${process.env.NEXT_PUBLIC_CLOUD_IMAGE_URL}w_280,h_398,c_fill,q_auto,fl_any_format,dpr_auto/blazing/${SeoData?.image_path}/${SeoData?.preview_image}`}
+          />
+        </Head>
+      )}
       <div className="streaming-screen">
         {isMobile ? (
           <div className="stream-top-header">
@@ -67,17 +73,17 @@ const StreamingPage = (data) => {
       </div>
     </>
   );
-};
-
-export async function getServerSideProps(context) {
-  // Fetch data from external API
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/stream/get-public-stream?uuid=${context.query.uuid}`
-  );
-  const data = await res.json();
-
-  // Pass data to the page via props
-  return { props: { data } };
 }
+
+// export async function getServerSideProps(context) {
+//   // Fetch data from external API
+//   const res = context.query.uuid && await fetch(
+//     `${process.env.NEXT_PUBLIC_API_URL}/stream/get-public-stream?uuid=${context.query.uuid}`
+//   );
+//   const data = await res.json();
+
+//   // Pass data to the page via props
+//   return { props: { data } };
+// }
 
 export default StreamingPage;
