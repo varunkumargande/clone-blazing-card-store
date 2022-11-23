5f4ff01f45af541c7b20612ba7900b4802d46ded
Varun Kumar Gande <babluvarun@gmail.com>
2022-11-23 19:23:20 +0530

    fixes

--- pages/streaming/index.js
+++ pages/streaming/index.js
@@ -8,13 +8,13 @@ import dynamic from "next/dynamic";
 import Footer from "../../components/partials/LandingPage/Footer";
 import IconBack from "../../components/Icons/IconBack";
 import { useIsMobile } from "../../contexts/Devices/CurrentDevices";
-import Head from "next/head";
+// import Head from "next/head";
 const Streaming = dynamic(() => import("../../components/elements/streaming"), {
   ssr: false,
 });
 
 function StreamingPage(data) {
-//   const SeoData = data?.data?.data;
+  //   const SeoData = data?.data?.data;
   const network = useNetwork();
   const { isMobile } = useIsMobile();
   const router = useRouter();
@@ -32,23 +32,23 @@ function StreamingPage(data) {
 
   return (
     <>
-    //   <Head>
-    //     <title>{SeoData?.title}</title>
-    //     <meta name="description" content={SeoData?.description} />
-    //     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    //     <meta property="og:title" content={SeoData?.title} />
-    //     <meta property="og:description" content={SeoData?.description} />
-    //     <meta
-    //       property="og:image"
-    //       content={`${process.env.NEXT_PUBLIC_CLOUD_IMAGE_URL}w_280,h_398,c_fill,q_auto,fl_any_format,dpr_auto/blazing/${SeoData?.image_path}/${SeoData?.preview_image}`}
-    //     />
-    //     <meta property="og:image:width" content="560" />
-    //     <meta property="og:image:height" content="796" />
-    //     <link
-    //       rel="shortcut icon"
-    //       href={`${process.env.NEXT_PUBLIC_CLOUD_IMAGE_URL}w_280,h_398,c_fill,q_auto,fl_any_format,dpr_auto/blazing/${SeoData?.image_path}/${SeoData?.preview_image}`}
-    //     />
-    //   </Head>
+      {/* <Head>
+        <title>{SeoData?.title}</title>
+        <meta name="description" content={SeoData?.description} />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content={SeoData?.title} />
+        <meta property="og:description" content={SeoData?.description} />
+        <meta
+          property="og:image"
+          content={`${process.env.NEXT_PUBLIC_CLOUD_IMAGE_URL}w_280,h_398,c_fill,q_auto,fl_any_format,dpr_auto/blazing/${SeoData?.image_path}/${SeoData?.preview_image}`}
+        />
+        <meta property="og:image:width" content="560" />
+        <meta property="og:image:height" content="796" />
+        <link
+          rel="shortcut icon"
+          href={`${process.env.NEXT_PUBLIC_CLOUD_IMAGE_URL}w_280,h_398,c_fill,q_auto,fl_any_format,dpr_auto/blazing/${SeoData?.image_path}/${SeoData?.preview_image}`}
+        />
+      </Head> */}
       <div className="streaming-screen">
         {isMobile ? (
           <div className="stream-top-header">
