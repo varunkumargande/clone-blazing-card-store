3c425f2abff1adbeab5a775ac839430743ca75e8
Varun Kumar Gande <babluvarun@gmail.com>
2022-11-25 12:46:19 +0530

    size blur bug

--- components/Constants/imageConstants.js
+++ components/Constants/imageConstants.js
@@ -2,12 +2,12 @@ export const ImageTransformation = {
   card: "w_280,h_398,c_fill,q_auto,fl_any_format,dpr_auto",
   streamThumnail: "w_592,h_730,c_fill,q_auto,fl_any_format,dpr_auto",
   profilePageImage: "w_172,h_172,c_fill,q_auto,g_auto,r_100,fl_any_format,dpr_auto",
-  profileImageHeader: "w_32,h_32,c_fill,q_auto,g_auto,fl_any_format,dpr_auto",
-  profileImageCard: "w_24,h_24,c_fill,q_auto,g_auto,fl_any_format,dpr_auto",
+  profileImageHeader: "w_132,h_132,c_fill,q_auto,g_auto,fl_any_format,dpr_auto",
+  profileImageCard: "w_124,h_124,c_fill,q_auto,g_auto,fl_any_format,dpr_auto",
   followersCard: "w_115,h_115,c_fill,q_auto,fl_any_format,dpr_auto",
   ProfileImage: "w_124,h_124,c_fill,r_100,q_auto,g_auto,fl_any_format,dpr_auto",
-  streamPageProfile: "w_32,h_32,c_fill,q_auto,g_auto,r_100,fl_any_format,dpr_auto",
-  streamChatProfile: "w_50,h_50,c_fill,q_auto,g_auto,r_100,fl_any_format,dpr_auto",
+  streamPageProfile: "w_132,h_132,c_fill,q_auto,g_auto,r_100,fl_any_format,dpr_auto",
+  streamChatProfile: "w_150,h_150,c_fill,q_auto,g_auto,r_100,fl_any_format,dpr_auto",
   sellerDocument: "w_206,h_130,c_fill,q_auto,fl_any_format,dpr_auto",
 };
 
