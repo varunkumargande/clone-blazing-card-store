19ada7679afa1274e9893e4e6d9388316768ee41
Varun Kumar Gande <babluvarun@gmail.com>
2022-12-02 12:14:43 +0530

    username bug

--- components/partials/LandingPage/Electronic.jsx
+++ components/partials/LandingPage/Electronic.jsx
@@ -3,7 +3,6 @@ import { stringFormatter } from "../../../utilities/utils";
 import Router from "next/router";
 import StreamCard from "../../elements/StreamCard";
 import { connect } from "react-redux";
-import { limit } from "../../Constants";
 import { showCatCardLoader } from "../../../api/utils/showCatCardLoader";
 import ShowViewAll from "../../reusable/viewAll";
 import StreamCardSkeleton from "../../../skeleton/StreamCardSkeleton";
