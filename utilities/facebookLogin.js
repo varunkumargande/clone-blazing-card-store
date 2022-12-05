3b201602656cefadc68235e3149abcbd8b96de5c
Gande Varun Kumar <gande.kumar@kelltontech.com>
2022-12-05 12:31:50 +0000

    deploying uat

---
+++ utilities/facebookLogin.js
@@ -0,0 +1,25 @@
+import React, { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import ReactDOM from "react-dom";
+import FacebookLogin from "react-facebook-login";
+import { facebookLoginApi } from "../api/auth/facebookLoginApi";
+
+const FacebookLoginComponent = () => {
+  const dispatch = useDispatch();
+  const responseFacebook = useCallback((response) => {
+    facebookLoginApi(response, dispatch);
+  });
+
+  return (
+    <div className="text-center mt-2">
+      <FacebookLogin
+        appId={process.env.NEXT_PUBLIC_FACEBOOK_KEY_ID}
+        fields="name,email,picture"
+        callback={responseFacebook}
+        icon="fa-facebook"
+      />
+    </div>
+  );
+};
+
+export default FacebookLoginComponent;
