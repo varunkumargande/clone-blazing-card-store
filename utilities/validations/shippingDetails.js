9ea5284f0e32194ac4dfa3d9bc0bf5b2d80f5f79
garima.dadheech <garima.dadheech@kelltontech.com>
2022-12-09 15:15:07 +0530

    made address line2 not required on become seller

--- utilities/validations/shippingDetails.js
+++ utilities/validations/shippingDetails.js
@@ -5,7 +5,7 @@ export const shippingDetailsvalidation = yup.object().shape({
   firstName: yup.string().matches(regex.onlyAlphabetsBothCases, 'Please enter letters only').required('Required'),
   lastName: yup.string().matches(regex.onlyAlphabetsBothCases, 'Please enter letters only').required('Required'),
   addressLine1: yup.string().required('Required'),
-  addressLine2: yup.string().required('Required'),
+  addressLine2: yup.string(),
   postalCode: yup.number().required('Required').typeError('Please enter digits only'),
   country: yup.string().required('Required'),
   city: yup.string().required('Required'),
